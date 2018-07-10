424d60002b495a7d2c0a1de60f91772fb44f9e5c
Doug Simon <doug.simon@oracle.com>
2018-07-10 13:36:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+A tool for creating a src.zip containing *all* the Java sources for Java classes in a JDK.
+Only the JDK source layout introduced in JDK 10 is currently supported.
---
+++ make-jdk-src-archive.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# Utility for creating a src.zip from *all* the Java source files in a JDK workspace.
+#
+
+#set -x
+
+if [ $# -eq 2 ]; then
+    archive_dir=$(dirname $2)
+    mkdir -p $archive_dir || { echo "Could not create $archive_dir"; exit 1; }
+    pushd $archive_dir >/dev/null
+    src_zip=$(/bin/pwd)/$(basename $2)
+    popd >/dev/null
+
+    if [ -e $src_zip ]; then
+        echo "Cannot overwrite $src_zip - please delete it first"
+        exit 1
+    fi
+    jdk_root=$1
+elif [ $# -eq 1 ]; then
+    jdk_tag=$1
+    src_zip=${PWD}/${jdk_tag}.src.zip
+    url=http://hg.openjdk.java.net/jdk/jdk/archive/${jdk_tag}.zip/src/
+    if [ ! -e jdk-${jdk_tag}.zip ]; then
+        echo "Downloading $url"
+        wget -O jdk-${jdk_tag}.zip $url
+    fi
+    if [ -e jdk-${jdk_tag} ]; then
+        echo "Removing jdk-${jdk_tag} ..."
+        rm -rf jdk-${jdk_tag}
+    fi
+    echo "Unzipping jdk-${jdk_tag}.zip ..."
+    unzip -q jdk-${jdk_tag}.zip "jdk-${jdk_tag}/src/*"
+    jdk_root=${PWD}/jdk-${jdk_tag}
+else
+    echo "Usage: $0 <JDK workspace> <zipfile>"
+    echo "   or: $0 <JDK tag>"
+    exit 1;
+fi
+
+pushd $jdk_root >/dev/null
+dirs=""
+if [ -d src/jdk.internal.vm.ci ]; then
+    # JDK >= 10
+    for d in $(find src -name classes -a -type d); do
+        alt_layout_dirs=$(ls -d $d/*/src 2>/dev/null)
+        if [ -n "$alt_layout_dirs" ]; then
+            dirs="$dirs $alt_layout_dirs"
+        else
+            dirs="$dirs $d"
+        fi
+    done
+else
+    echo "Unsupported/unknown JDK layout"
+    exit 1
+fi
+
+for classes_dir in $dirs; do
+    echo "Adding sources from $classes_dir to $src_zip ..."
+    pushd $classes_dir >/dev/null
+    zip -r -q -u $src_zip . -i \*.java -x \*SCCS\*
+    popd >/dev/null
+done
+popd >/dev/null
+
+if type -p shasum >/dev/null ; then
+    shasum ${src_zip} | awk '{print $1}' > ${src_zip}.sha1
+else
+    sha1sum ${src_zip} | awk '{print $1}' > ${src_zip}.sha1
+fi
+
+if [ $# -eq 1 ]; then
+    rm -rf jdk-${jdk_tag}
+fi
